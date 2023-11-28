3184ffaf1704abd68425f8713bdec0d34856541c
Lucas Lam <44491128+lucas-slam@users.noreply.github.com>
2023-11-28 09:14:11 +0000

    plotter.py: show nodes distribution in dataset

---
+++ src/plotter.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import Dataset
+import file_util as fu
+
+"""
+    function name: plot_distribution
+    params:
+        file_name (str): file name in array
+        dataset (Dataset): the dataset
+"""
+def plot_distribution(file_name, dataset):
+    coords_x = [dataset.coord_x[1:len(dataset.coord_x)]]
+    coords_y = [dataset.coord_y[1:len(dataset.coord_x)]]
+    plt.scatter(coords_x, coords_y, s=0.5)
+    plt.show()
+
+# Testing Section
+ds = fu.file_reader(0)
+plot_distribution(None, ds)
