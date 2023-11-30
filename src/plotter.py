de0d36cc9a891fae0ab253ffee876734a705fa0b
Lucas Lam <44491128+lucas-slam@users.noreply.github.com>
2023-11-30 14:48:02 +0000

    plotter.py: added colormap into nodes

--- src/plotter.py
+++ src/plotter.py
@@ -9,12 +9,18 @@ import file_util as fu
         dataset (Dataset): the dataset
 """
 def plot_distribution(file_name, dataset):
-    coords_x = [dataset.coord_x[1:len(dataset.coord_x)]]
-    coords_y = [dataset.coord_y[1:len(dataset.coord_x)]]
-    plt.scatter(coords_x, coords_y, s=0.5)
+    coords_x = [dataset.coord_x[1:]]
+    coords_y = [dataset.coord_y[1:]]
+    node_items_highest_profit_ratio = [0]
+    for i, node in enumerate(dataset.nodes):
+        if i > 1:
+            node_items_highest_profit_ratio.append(node.items[0].profit_ratio)
+    plt.figure(figsize=(14,10))
+    plt.scatter(coords_x, coords_y, c=node_items_highest_profit_ratio, s=20, cmap='viridis')
+    plt.colorbar()
     plt.show()
 
 
 # Testing Section
-ds = fu.file_reader(1)
+ds = fu.file_reader(2)
 plot_distribution(None, ds)
