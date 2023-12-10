a509ecf8b1b1130e77f2b5cc889f49b34ea98175
Lucas Lam <lucaslam990202@gmail.com>
2023-12-10 16:25:09 +0000

    first merge

--- src/file_util.py
+++ src/file_util.py
@@ -2,6 +2,7 @@
 import Dataset
 import Item
 import Node
+import Params
 
 folder_path = "/Users/lucaslam/PycharmProjects/pythonProject/NIC_CA2_group_project/resources/"
 # folder_path = "C:/Users/Lucas/Documents/Academic/Exeter/ECMM409 Nature Inspired Computation/CA2/CA2PY/resources/"
@@ -9,6 +10,7 @@ file_names = ["a280-n279", "a280-n1395", "a280-n2790",
               "fnl4461-n4460", "fnl4461-n22300", "fnl4461-n44600",
               "pla33810-n33809", "pla33810-n169045", "pla33810-n338090",
               "test-example-n4"]
+param_file = "param_properties"
 file_ext = ".txt"
 
 """
@@ -43,7 +45,7 @@ def file_reader(selected_file):
         dataset.coord_y.append(node_y)
 
     item_line_offset = node_line_offset + dataset.dimension + 1
-    dataset.items = [[]] * (dataset.dimension + 1)
+    dataset.items = [None]#[[]] * (dataset.dimension + 1)
     # inx 0 not used in list
     dataset.items[0] = [None]
     for i in range(dataset.num_of_items):
@@ -52,9 +54,10 @@ def file_reader(selected_file):
         weight = float(lines[item_line_offset + i].split(splitter)[2])
         assigned_node = int(lines[item_line_offset + i].split(splitter)[3])
         item = Item.Item(index, profit, weight, assigned_node)
-        node_items = dataset.items[assigned_node].copy()
-        node_items.append(item)
-        dataset.items[assigned_node] = node_items
+        dataset.items.append(item)
+        #node_items = dataset.items[assigned_node].copy()
+        #node_items.append(item)
+        #dataset.items[assigned_node] = node_items
         dataset.nodes[assigned_node].items.append(item)
 
     for i in range(len(dataset.nodes)):
@@ -63,3 +66,22 @@ def file_reader(selected_file):
             node.sort_item_profit_ratio()
 
     return dataset
+
+def read_param_properties():
+    full_path = folder_path + param_file + file_ext
+    splitter = " "
+    print("Reading:" + full_path)
+    params = Params.Param()
+    with open(full_path, mode='r') as f:
+        lines = f.read().splitlines()
+    params.population_size_nsg = int(lines[0].split(splitter)[1])
+    params.evaluations_tsp = int(lines[1].split(splitter)[1])
+    params.run_local_tsp = bool(lines[2].split(splitter)[1])
+    params.tournament_size_ksp = int(lines[3].split(splitter)[1])
+    params.num_generations_ksp = int(lines[4].split(splitter)[1])
+    params.fill_rate_ksp = float(lines[5].split(splitter)[1])
+
+    if params.population_size_nsg < params.tournament_size_ksp:
+        raise Exception("Tournament of KSP size should be smaller than population size")
+
+    return params
