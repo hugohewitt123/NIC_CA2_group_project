2617f087413873afca69d02611a98ea1e27caf71
Lucas Lam <44491128+lucas-slam@users.noreply.github.com>
2023-12-11 19:23:25 +0000

    change README.md

--- src/pack_util.py
+++ src/pack_util.py
@@ -14,10 +14,6 @@ def node_dist(node1, node2):
     z = math.sqrt(z)
     return(z)
 
-def check_problem_constraints(knapsack_capacity):
-    '''function to check that the problem constraints aren't violated'''
-    ###TODO###
-
 def velocity(w, cap):
     '''funcition to work out the current velocity'''
     return 1.0 - ((1-0.1)/cap)*w
@@ -28,6 +24,7 @@ def tournament_selection(population, profits, weights, tournament_size, knapsack
     N2 = []
     visited1 = []
     visited2 = []
+    #selecting a number random items from the population depending on the tournament size
     for i in range(tournament_size):
         randchoice1 = random.choice([i for i in range(0,len(population)) if i not in visited1])
         randchoice2 = random.choice([i for i in range(0,len(population)) if i not in visited2])
@@ -35,7 +32,9 @@ def tournament_selection(population, profits, weights, tournament_size, knapsack
         N2.append(randchoice2)
         visited1.append(randchoice1)
         visited2.append(randchoice2)
-    a = b = a_i = b_i = 0
+    #choosing the items in the tournament with the best profit
+        #(given the knapsack capacity isn't violated)
+    a = b = a_i = b_i = -1
     for i in N1:
         if profits[i] > a and weights[i] < knapsack_cap:
             a = profits[i]
@@ -55,6 +54,142 @@ def single_point_crossover(a, b):
 
     return c, d
 
+def two_point_crossover(a, b):
+    '''Function to perform two-point crossover on the knapsack'''
+    x, y = sorted(random.sample(range(1, len(a)-1), 2))
+    c = a[:x] + b[x:y] + a[y:]
+    d = b[:x] + a[x:y] + b[y:]
+
+    return c, d
+
+def three_point_crossover(a, b):
+    '''Function to perform three-point crossover on the knapsack'''
+    x, y, z = sorted(random.sample(range(1, len(a)-1), 3))
+    c = a[:x] + b[x:y] + a[y:z] + b[z:]
+    d = b[:x] + a[x:y] + b[y:z] + a[z:]
+
+    return c, d
+
+def random_point_crossover(a, b):
+    '''Function to perform random-point crossover on the knapsack'''
+    num_points = random.randint(1, len(a)-1)
+    points = sorted(random.sample(range(1, len(a)), num_points))
+
+    c, d = a[:], b[:]
+    switch = False
+    for point in points:
+        if switch:
+            c[point:], d[point:] = d[point:], c[point:]
+        switch = not switch
+
+    return c, d
+
+def binary_mask_crossover(a, b):
+    mask = [random.choice([0, 1]) for _ in range(len(a))]
+    c = [a[i] if mask[i] == 0 else b[i] for i in range(len(a))]
+    d = [b[i] if mask[i] == 0 else a[i] for i in range(len(b))]
+    return c, d
+
+def blend_crossover(a, b, alpha=0.5):
+    c = [a[i] if random.random() < alpha else b[i] for i in range(len(a))]
+    d = [b[i] if random.random() < alpha else a[i] for i in range(len(b))]
+    return c, d
+
+def simulated_binary_crossover(a, b, eta=2, prob_cross=0.9):
+    c = list(map(int, a))
+    d = list(map(int, b))
+
+    if random.random() < prob_cross:
+        for i in range(len(a)):
+            if random.random() < 0.5:
+                beta = (2.0 * random.random())**(1.0 / (eta + 1))
+                c[i] = int(0.5 * (((1 + beta) * a[i]) + (1 - beta) * b[i]))
+                d[i] = int(0.5 * (((1 - beta) * a[i]) + (1 + beta) * b[i]))
+
+    return c, d
+
+def ordered_crossover(a, b):
+    # Randomly select two crossover points
+    points = sorted(random.sample(range(len(a)), 2))
+
+    # Create masks to preserve selected segments
+    mask_a = [False] * len(a)
+    mask_b = [False] * len(b)
+
+    # Mark selected segments in the masks
+    for i in range(points[0], points[1]):
+        mask_a[i] = True
+        mask_b[i] = True
+
+    # Get segments for offspring by copying the selected segments
+    segment_a = [gene for gene, mask in zip(b, mask_a) if not mask]
+    segment_b = [gene for gene, mask in zip(a, mask_b) if not mask]
+
+    # Create offspring by combining segments and preserving order
+    c = segment_a[:points[0]] + a[points[0]:points[1]] + segment_a[points[0]:]
+    d = segment_b[:points[0]] + b[points[0]:points[1]] + segment_b[points[0]:]
+
+    return c, d
+
+def cycle_crossover(a, b):
+    size = len(a)
+    cycle = [False] * size
+    child1, child2 = [-1] * size, [-1] * size
+
+    # Start with the first cycle
+    index = 0
+    while not cycle[index]:
+        cycle[index] = True
+
+        child1[index] = a[index]
+        child2[index] = b[index]
+
+        value_b = b[index]
+        index = a.index(value_b)
+
+    # Fill in remaining elements
+    for i in range(size):
+        if not cycle[i]:
+            child1[i] = b[i]
+            child2[i] = a[i]
+
+    return child1, child2
+
+def displacement_crossover(a, b):
+    # Randomly select a crossover segment
+    segment_length = random.randint(1, min(len(a), len(b)) - 1)
+    start_index = random.randint(0, min(len(a), len(b)) - segment_length)
+
+    # Extract the selected segment from both parents
+    segment_a = a[start_index:start_index + segment_length]
+    segment_b = b[start_index:start_index + segment_length]
+
+    # Create offspring by replacing the segment with the other parent's segment
+    c = a[:start_index] + segment_b + a[start_index + segment_length:]
+    d = b[:start_index] + segment_a + b[start_index + segment_length:]
+
+    return c, d
+
+def insertion_mutation(c, d):
+    # Select a random element from chromosome 'c'
+    element = random.choice(c)
+    # Remove the selected element from chromosome 'c'
+    c.remove(element)
+    # Choose a random position in chromosome 'd' to insert the element
+    insert_position = random.randint(0, len(d))
+    d.insert(insert_position, element)
+
+    # Select a random element from chromosome 'd'
+    element = random.choice(d)
+    # Remove the selected element from chromosome 'd'
+    d.remove(element)
+    # Choose a random position in chromosome 'd' to insert the element
+    insert_position = random.randint(0, len(c))
+    c.insert(insert_position, element)
+
+    return c, d
+
+
 def bitflip_mutation(c, d):
     '''function to perform bitflip mutation'''
     i = random.randint(0,len(c)-1)
@@ -69,6 +204,7 @@ def generate_random_population(population_size, num_of_items, ds, knapsack_cap,f
     for i in range(population_size):
         lst = [0]*num_of_items
         weight = 0
+        #filling the knapsack until the weight capacity is reached
         while weight < knapsack_cap*fill_rate:
             rnd = random.randint(0,num_of_items-1)
             if lst[rnd] == 0:
@@ -82,6 +218,7 @@ def evaluate_pack(a, ds):
     p = 0
     w = 0
     for i in range(len(a)):
+        #if the pack plan picks up the item (1) get the weight and profit from the dataset
         if a[i] == 1:
             w += ds.items[i+1].weight
             p += ds.items[i+1].profit
@@ -91,6 +228,7 @@ def get_profit_weights(population, ds):
     '''function to get the total weight of the population'''
     profits = []
     weights = []
+    #evaluates the weight and profit for the whole population
     for pop in population:
         p = 0
         w = 0
